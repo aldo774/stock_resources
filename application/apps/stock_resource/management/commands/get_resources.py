9e983953885e72ccbfb3de8a07e85d59d797de08
Aldo Soares <soares_aldo@hotmail.com>
2020-03-27 22:11:19 -0300

    Implements get resources command

---
+++ application/apps/stock_resource/management/commands/get_resources.py
@@ -0,0 +1,44 @@
+import json
+import logging
+import random
+import requests
+import time
+from lxml import html
+
+from django.core.management import BaseCommand
+
+from application.apps.stock_resource.models import Stock
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = "Gets stock resources"
+
+    def handle(self, *args, **options):
+        logger.info("Getting resources")
+        stocks = Stock.objects.all()
+        for stock in stocks:
+            interval_request = random.randrange(2000, 3000)/1000
+            time.sleep(interval_request)
+
+            if '{}' in stock.site.url:
+                url = stock.site.url.format(stock.name)
+            else:
+                url = stock.site.url + stock.name
+            
+            logger.info(f"Accessing {stock.name} resources...")
+            page = requests.get(url)
+
+            if page.status_code not in (200, 201):
+                logger.info(f"Content from {stock.name} denied")
+
+            tree = html.fromstring(page.content)
+            values = {}
+
+            for resource in stock.site.resources.all():
+                res = tree.xpath(resource.xpath)
+                values[resource.label] = res[0] if res else ''
+
+            stock.resources_value = values
+            stock.save()
