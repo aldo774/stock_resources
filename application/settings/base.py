8fb93d5ad164833bd16197a4d8fd1184ecae09ae
Aldo Soares <soares_aldo@hotmail.com>
2020-03-29 01:19:20 -0300

    Creates cron job to execute periodically command 'get_resources'

--- application/settings/base.py
+++ application/settings/base.py
@@ -78,22 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'application.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'DB_ENGINE',
-        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'DB_NAME',  # Or path to database file if using sqlite3.
-        'USER': 'DB_USER',  # Not used with sqlite3.
-        # 'PASSWORD': DB_PASS,  # Not used with sqlite3.
-        'HOST': 'DB_HOST',  # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': 'DB_PORT',  # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
---
+++ scripts/get_resources.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/usr/local/bin/python3 /code/manage.py get_resources --settings=$1
