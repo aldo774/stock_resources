72592e2c967c30fd3266f64283d744a18ec33788
Aldo Soares <soares_aldo@hotmail.com>
2020-07-06 23:13:37 -0300

    Adds CORS middleware

--- application/settings/base.py
+++ application/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     DrfConfig.name,
     StockResource.name
 
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'application.urls'
