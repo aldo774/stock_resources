93ba1eeb0424ef2f8da6ab1a02eeb1733a99b55a
Aldo Soares <soares_aldo@hotmail.com>
2020-07-10 17:27:36 -0300

    Changes frequency in cron

--- docker/dev/cron/entrypoint.sh
+++ docker/dev/cron/entrypoint.sh
@@ -8,7 +8,7 @@ echo "Creating cronjob to get resources"
 mkdir /code/logs/
 touch /code/logs/crontab.log
 chmod +x /code/scripts/get_resources.sh
-echo "* 3 * * * /code/scripts/get_resources.sh application.settings.dev >> /code/logs/crontab.log 2>&1" > /etc/crontab
+echo "0 5 * * 1,2,3,4,5 /code/scripts/get_resources.sh application.settings.dev >> /code/logs/crontab.log 2>&1" > /etc/crontab
 crontab /etc/crontab
 /usr/sbin/service cron start
 tail -f /code/logs/crontab.log
